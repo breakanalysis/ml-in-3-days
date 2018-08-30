6e463a668c56fab43a3407f6c7fe4f9d541f27d8
Jacob Sznajdman <breakanalysis@gmail.com>
2018-08-30 22:04:18 +0200

    rename

--- cheatdiy/setup.py
+++ cheatdiy/setup.py
@@ -3,10 +3,10 @@ import setuptools
 description = "Cheats and Hints for mlworkshop"
 
 setuptools.setup(
-    name="cheat-diy",
+    name="cheatdiy",
     version="1",
     author="Jacob Sznajdman",
     author_email="jacob@sznajdman.com",
     description=description,
-    packages=['cheat-diy']
+    packages=['cheatdiy']
 )

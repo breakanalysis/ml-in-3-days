1e49e071274876b1844349dbedec376d11942d95
Jacob Sznajdman <breakanalysis@gmail.com>
2018-08-31 12:23:33 +0200

    finished hints

--- cheatdiy/cheatdiy/cheatdiy.py
+++ cheatdiy/cheatdiy/cheatdiy.py
@@ -138,84 +138,44 @@ def hint(code):
                li = np.array([10,100,1000])
                li[[1,0,2]]
                for the np.random.shuffle method please see help or cheat.''',
-        11: '''# pick all rows and all columns excluding the last one
-               X = data[:,:-1]
+        11: '''# pick all rows and all columns excluding the last one. the index of the last column can be data.shape[1]-1 , or just -1.
+               X = data[__fill_in__,__fill_in__]
                # pick all rows and only last column
-               Y = data[:,-1]
-               reg.fit(X, Y)''',
-        12: '''pred = reg.predict(X)
-               acc = (pred == Y).sum()/2000''',
-        13: "cross_val_score(reg, X, Y, scoring='accuracy', cv=5)",
+               Y = __fill_in__ 
+               reg.fit(X,Y)''',
+        12: '''pred = reg.predict(__fill_in__)
+               for accuracy, you can first check when the predictions are correct with (pred==Y). you can then use .sum() for counting.''',
+        13: "you should use the arguments scoring='accuracy' and cv=5. you can use a full class for the crossvalidation object but cv=5 is a lot simpler.",
         14: '''# first cell:
-               sizes, train_scores, test_scores = learning_curve(LogisticRegression(), X, Y, cv=5, scoring='accuracy', train_sizes=np.linspace(0.1,1.0,200))
+               this is very similar to previous exercise 
                # second cell:
-               train_scores_mean = train_scores.mean(axis=1)
-               test_scores_mean = test_scores.mean(axis=1)
+               train_scores_mean = train_scores.mean(axis=__fill_in__)
+               test_scores_mean = test_scores.mean(axis=__fill_in__)
                # third cell:
                # like above but use scoring='neg_log_loss' instead of scoring='accuracy'.''',
         15: '''# loads hand written character image data
                digits = load_digits()
                X, y = digits.data, digits.target
         
-               params = np.logspace(-4,1,6)
-               train_scores, valid_scores = validation_curve(reg, X, y, 'C', params, cv=5,scoring='neg_log_loss')
-        
-               train_scores_mean = train_scores.mean(axis=1)
-               valid_scores_mean = valid_scores.mean(axis=1)
-               assert train_scores_mean.shape == (6,)
-               assert valid_scores_mean.shape == (6,)
+               params = np.logspace(first_exponent,last_exponent,number_of_values)
+               train_scores, valid_scores = validation_curve(reg, X, y, __string_name_of_the_parameter__, __the_values_of_the_parameter__, cv=5,scoring='neg_log_loss')
+               # compute means as previous exercise 
         
                plt.semilogx(params, -train_scores_mean, c='red', label='train')
                plt.semilogx(params, -valid_scores_mean, c='blue', label='validation')
                plt.legend()''',
-        16: "parameters = [{'kernel':['poly'], 'degree':[1, 2,3]}, {'kernel': ['rbf'], 'C': [1,10]}, {'kernel': ['linear'], 'C': [1,10], 'gamma': [0.1,1]}]",
+        16: "parameters = [{'kernel':['poly'], 'degree':[1, 2,3]}, __continue_with_the_other_kernels__]",
         17: '''
                def dL(s):
-                     return np.exp(-s)''',
+                     return np.exp(__fill_in__)''',
         18: '''
                def h(X,w)
-                     return np.matmul(X,w)''',
+                     # return np.matmul(w,X)
+                     # or...
+                     # return np.matmul(X,w)''',
         19: '''X = data.copy()
-               X[:,2] = 1''',
-        20: '''# Learning rate, try different values if you want. What is the connection to epochs?
-               lr = 1.5
-               # Epochs is the number of passes through the data and also the number of weight updates.
-               epochs = 20
-
-               # x values for plotting
-               ticks = [-4 + 0.16*t for t in range(100)]
-
-               # plot data 
-               plt.scatter(twoD2[:,0], twoD2[:,1], c='pink')
-               plt.scatter(twoD[:,0], twoD[:,1], c='gray')
-
-               # initialization of weights
-               w = np.random.randn(3)
-
-               for epoch in range(epochs):
-                   # compute gradient
-                   scores = h(X,w)
-                   sign_scores = scores * data[:,2]
-                   acc = (sign_scores > 0).sum()/2000
-                   print('accuracy for epoch %s is %s' % (epoch, acc))
-                   example_weights = dL(sign_scores)
-                   median = np.median(example_weights)
-                   example_weights = np.array([w if w < median else median for w in example_weights])
-                   example_weights /= example_weights.sum()
-                   example_weights = example_weights * data[:,2]
-                   grad = np.matmul(X.T,example_weights)
-                   # update weights
-                   w += lr*grad
-                   a, b, c = w
-                   # y-values of the decision boundary line
-                   boundary_y = [-(a*x + c)/b for x in ticks]
-                   boundary_y = [b if b < 15 else 15 for b in boundary_y]
-                   boundary_y = [b if b > -15 else -15 for b in boundary_y]
-                   gray = 1-epoch/epochs
-                   plt.plot(ticks, boundary_y, label='epoch: ' + str(epoch), c = (gray,gray,gray))
-    
-               plt.legend()
-               plt.show()'''
+               X[:,__select_third_column__] = 1''',
+        20: "No hint given for this 'bonus' exercise." 
     }
     if code in hint_dict:
         print(inspect.cleandoc(hint_dict[code]))
